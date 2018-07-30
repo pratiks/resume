9a01ef9fde5d11bd91dbd02cb16d90134b2e126d
EC2 Default User <ec2-user@ip-172-31-21-13.us-east-2.compute.internal>
2018-07-30 18:10:30 +0000

    update to add cloud9 config.

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ let express = require('express')
 let app = express()
 let c9config = require("c9config"); 
 
-app.set('port', (process.env.PORT || 8080))
+process.env.PORT = process.env.PORT || 8080;
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.render("./cv/index.html")
+app.get('/', (req, res) => {
+  res.send("test")
 })
 
-app.listen(app.get('port'), function() {
-console.log("Node app is running at localhost:" + app.get('port'))
 
-c9config.isC9 // true or false depending wether on C9 or not. 
-c9config.print() // prints an overview of the important information to console 
-c9config.workspace
-  
-})
+app.listen(process.env.PORT, function () {
+	console.log(`Starting Server @ port ${process.env.PORT}....`);
+	console.log(`Server now listening on port ${process.env.PORT} successfully!`);
+	c9config.isC9 // true or false depending wether on C9 or not. 
+    c9config.print() // prints an overview of the important information to console 
+    c9config.workspace
+});
