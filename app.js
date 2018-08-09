f631dcc8354da6f2fc221eed8b4826466760fb9e
Pratik Shah <pshah@atlassian.com>
2018-08-09 13:35:06 -0500

    update to listen method.

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 let express = require('express')
 let app = express()
-let c9config = require("c9config"); 
 
 process.env.PORT = process.env.PORT || 8080;
 app.use(express.static(__dirname + '/public'))
@@ -11,11 +10,6 @@ app.get('/', (req, res) => {
   res.redirect(__dirname + "/resume/index.html")
 })
 
-
-app.listen(process.env.PORT, function () {
-	console.log(`Starting Server @ port ${process.env.PORT}....`);
-	console.log(`Server now listening on port ${process.env.PORT} successfully!`);
-	c9config.isC9 // true or false depending wether on C9 or not. 
-    c9config.print() // prints an overview of the important information to console 
-    c9config.workspace
+app.listen(process.env.PORT, () => {
+  console.log(`\n Server now listening at ${process.env.PORT} successfully! \n`);
 });
